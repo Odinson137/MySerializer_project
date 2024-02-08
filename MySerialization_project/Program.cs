841915c8eac90a19afeb1f95b5b2d3a0655905e3
Yura buryy <buryy137@gmail.com>
2024-02-08 21:23:34 +0300

    Добавьте файлы проекта.

---
+++ MySerialization_project/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using MySerialization_project;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+
+var model = new Test
+{
+    Salary = new Test2()
+    {
+        Salary = 143,
+    },
+    Height = 123,
+    Name = "Hello",
+    LastName = "Hello",
+};
+
+Stopwatch stopwatch = new Stopwatch();
+
+stopwatch.Start();
+
+var str = Selializer.Serialize(new List<Test>() { model }).ToString();
+
+stopwatch.Stop();
+
+Console.WriteLine($"Время выполнения моего кода: {stopwatch.ElapsedMilliseconds} миллисекунд.");
+
+stopwatch.Start();
+
+var str2 = JsonConvert.SerializeObject(model);
+
+stopwatch.Stop();
+
+Console.WriteLine($"Время выполнения внешней библиотеки: {stopwatch.ElapsedMilliseconds} миллисекунд.");
+
+stopwatch.Start();
+
+stopwatch.Stop();
+
+Console.WriteLine($"Время выполнения внешней библиотеки: {stopwatch.ElapsedMilliseconds} миллисекунд.");
+
+
+Console.WriteLine(str);
+Console.WriteLine(str2);
+
+
+class Test
+{
+    public bool IsActive { get; set; } = true;
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public int Height { get; set; }
+    public Test2 Salary { get; set; }
+}
+
+class Test2
+{
+    public Test3 Age { get; set; }
+    public int Salary { get; set; }
+}
+
+class Test3
+{
+    public int Age { get; set; }
+}
---
+++ MySerialization_project/Selializer.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace MySerialization_project
+{
+    internal static class Selializer
+    {
+        public static StringBuilder Serialize<T>(T obj)
+        {
+            var type = typeof(T);
+            return Serialize(obj, type);
+        }
+
+        public static StringBuilder Serialize(object obj, Type type)
+        {
+            var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.Append('{');
+
+            foreach (var property in properties)
+            {
+                strBuilder.Append($",");
+                var value = property.GetValue(obj);
+
+                if (value == null)
+                {
+                    strBuilder.Append($"\"{property.Name}\":");
+                    strBuilder.Append($"null");
+                }
+                else
+                {
+                    var valueType = value.GetType();
+
+                    if (valueType.IsClass && valueType != typeof(string))
+                    {
+                        strBuilder.Append($"\"{property.Name}\":");
+                        var child = Serialize(value, valueType);
+                        strBuilder.Append(child);
+                    }
+                    else
+                    {
+                        strBuilder.Append($"\"{property.Name}\":");
+                        strBuilder.Append(GetTypeValue(valueType, value));
+                    }
+                }
+ 
+            }
+
+            strBuilder.Append('}');
+
+            strBuilder.Remove(1, 1);
+
+            return strBuilder;
+        }
+
+       private static object GetTypeValue(Type typeCode, object value)
+        {
+            switch (Type.GetTypeCode(typeCode))
+            {
+                case TypeCode.Boolean:
+                    return value.ToString().ToLower();
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.Int16:
+                case TypeCode.UInt16:
+                case TypeCode.Int32:
+                case TypeCode.UInt32:
+                case TypeCode.Int64:
+                case TypeCode.UInt64:
+                case TypeCode.Decimal:
+                case TypeCode.Single:
+                case TypeCode.Double:
+                    return value;
+                default:
+                    return $"\"{value}\"";
+            }
+        }
+    }
+}
